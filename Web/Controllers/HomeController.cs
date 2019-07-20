afb5a6e7e0a3f8bb0b4df43495dc2766933e56de
Çağdaş Ekşi <cagdas_eksioglu@hotmail.com>
2019-07-20 23:24:07 +0300

    Initial commit

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        u7666632_dbEkinZahireEntities db = new u7666632_dbEkinZahireEntities();
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserEntity model)
+        {
+
+            var user = new CeriderUser()
+            {
+                Adress = model.Adress,
+                BirthDate = model.BirthDate,
+                City = model.City,
+                CreatedDate = DateTime.Now,
+                Email = model.Email,
+                FullName = model.FullName,
+                Job = model.Job,
+                Phone = model.Phone,
+                Status = 0,
+                Town = model.Town
+            };
+
+            db.CeriderUsers.Add(user);
+            db.SaveChanges();
+
+            ViewBag.Message = string.Format("<div class=\"alert alert-success text-center\"><strong> ÇERİDER</strong> {0}.</div>", "Üyeliğiniz oluşturulmuştur Teşekkürler.");
+
             return View();
         }
 
---
+++ Web/Models/UserEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class UserEntity
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string BirthDate { get; set; }
+        public string Job { get; set; }
+        public string City { get; set; }
+        public string Town { get; set; }
+        public string Adress { get; set; }
+    }
+}
